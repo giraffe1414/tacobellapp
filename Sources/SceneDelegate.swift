ade2df8840c15a0a47a41a9d56e8c195149d5a91
case <casekfoster@gmail.com>
2025-05-07 18:40:20 -0600

    Fix SceneDelegate window setup

--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -4,7 +4,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard (scene as? UIWindowScene) != nil else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let viewController = storyboard.instantiateInitialViewController() {
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
