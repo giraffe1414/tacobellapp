f84cb152b933253798ee447c806fa8b92f2b4a18
case <casekfoster@gmail.com>
2025-05-07 18:47:00 -0600

    Fix duplicate method and add logging in AppDelegate

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -3,14 +3,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("AppDelegate: didFinishLaunching")
         return true
     }
 
     // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        print("AppDelegate: configurationForConnecting")
+        let sceneConfig = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        print("AppDelegate: didDiscardSceneSessions")
     }
 }
